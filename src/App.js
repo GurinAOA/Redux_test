4ba6a56e7675241b6dea2eccb5ba27030ddfca33
GurinAOA <kiulo.oksana@gmail.com>
2023-09-26 21:34:06 +0300

    add project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux'
-import {del} from './redux/action'
-import {put} from './redux/action'
-import './App.css';
-import Counter from './components/Counter';
-import Edit from './components/Edit';
-
-
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { del } from "./redux/action";
+import { put } from "./redux/action";
+import "./App.css";
+import Counter from "./components/Counter";
+import Edit from "./components/Edit";
 
 function App() {
-
-  const posts = useSelector((state) => state)
-  const dispatch = useDispatch()
+  const posts = useSelector((state) => state);
+  const dispatch = useDispatch();
   console.log(posts);
 
+  function delPost(id) {
+    dispatch(del(id));
+  }
 
- function delPost(id){
-  dispatch(del(id))
- }
-
-
- function editPost(obj,id){
-  dispatch(put(obj,id))}
-
-
+  function editPost(obj, id) {
+    dispatch(put(obj, id));
+  }
 
   return (
     <div className="App">
       <div className="title">Enter your data</div>
       <div>
-        <Counter id={posts.length ? posts[posts.length-1].id : null} />
+        <Counter id={posts.length ? posts[posts.length - 1].id : null} />
       </div>
       <ul>
         {posts?.map((item, index) => (
@@ -37,18 +31,30 @@ function App() {
             <p className="item">{item.title}</p>
             <p className="item">{item.text}</p>
             <p className="item">{item.status}</p>
-            <button className="btn" onClick={()=>{delPost(item.id)}} >DELPOST</button>
+            <button
+              className="btn"
+              onClick={() => {
+                delPost(item.id);
+              }}
+            >
+              DELPOST
+            </button>
 
-<div>
-<Edit id={posts.length ? posts[posts.length-1].id : null} />
-</div>
+            <div>
+              <Edit id={posts.length ? posts[posts.length - 1].id : null} />
+            </div>
 
-
-            <button className="btn" onClick={()=>{editPost(item.id)}}>EDITPOST</button>
+            <button
+              className="btn"
+              onClick={() => {
+                editPost(item.id);
+              }}
+            >
+              EDITPOST
+            </button>
           </li>
         ))}
       </ul>
-
     </div>
   );
 }
